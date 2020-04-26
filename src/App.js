03f8f3337338ecbaa4f50779c20fb35a092952ad
Aabha <aabha.khandwala@gmail.com>
2020-04-25 22:31:58 -0400

    with css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
 import moment from "moment";
+import "moment-timezone";
+//import tz from "zipcode-to-timezone";
+import "./App.css";
 
 class App extends Component {
   state = {};
   getTime = () => {
-    let time = moment().format("llll");
-    console.log(time);
+    //const zone = tz.lookup(this.state.zip);
+    //const zone = timezone.lookup(this.state.zip);
+    //const zone = this.state.timezone / 60;
+    //console.log(zone);
+    let time = moment()
+      .utcOffset(this.state.timezone / 60)
+      .format("YYYY-MM-DD hh:mm a ");
 
     this.setState({
       time: time,
@@ -17,11 +25,12 @@ class App extends Component {
     console.log(zipInput);
     let apiKey = process.env.REACT_APP_WEATHER_API_KEY;
     console.log(apiKey);
+
     fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=` +
         zipInput +
-        `,US&units=imperial&appid=4890ec8c1a44083580937d9c9371b03a`
-      //process.env.REACT_APP_WEATHER_API_KEY
+        `,US&units=imperial&appid=` +
+        process.env.REACT_APP_WEATHER_API_KEY
     )
       .then((response) => {
         if (response.status !== 200) {
@@ -33,8 +42,12 @@ class App extends Component {
         response.json().then((data) => {
           console.log(data);
           this.setState({
-            temperature: data.main.temp,
+            zip: data.zipInput,
+            temperature: Math.round(data.main.temp) + "°F",
             city: data.name,
+            timezone: data.timezone,
+            description: data.weather[0].description,
+            icon: data.weather[0].icon,
           });
           this.getTime();
         });
@@ -47,20 +60,63 @@ class App extends Component {
   render() {
     return (
       <>
-        <div>
-          <input
-            type="text"
-            placeholder="Enter ZipCode Here"
-            id="zipcode"
-          ></input>
-          <button onClick={this.getWeather}>Go!</button>
-        </div>
-        <div>
-          <p>{this.state.temperature}</p>
-          <p>{this.state.city}</p>
-          <p>{this.state.time}</p>
+        <div class="container">
+          <h2>WEATHER APP</h2>
+
+          <div id="header">
+            <input
+              type="text"
+              placeholder="Enter ZipCode Here"
+              id="zipcode"
+            ></input>
+            <button id="btn" onClick={this.getWeather}>
+              Go!
+            </button>
+          </div>
+
+          <div id="main">
+            <section class="location">
+              <div id="city">{this.state.city}</div>
+              <div class="time">{this.state.time}</div>
+            </section>
+            <div class="current">
+              <div id="temp">
+                {this.state.temperature}
+                <span></span>
+              </div>
+              <div id="desc">{this.state.description}</div>
+              <img
+                src={`http://openweathermap.org/img/w/${this.state.icon}.png`}
+                alt=""
+              ></img>
+            </div>
+          </div>
         </div>
       </>
+      //   <div  class="container">
+      //   <h2>WEATHER APP</h2>
+      //  <!-- container for Moment.js output -->
+      //     <!----- <div id="displayMoment"></div>-->
+      //  <header>
+      //      <input id="zipinpt" type="text" placeholder="Enter ZipCode"></INput>
+      //      <button id="btn" >Go!</button>
+      //  </header>
+      //  <main>
+      //      <section class="location">
+      //          <div id="city"></div>
+      //          <div class="time"></div>
+      //      </section>
+      //      <div class=current>
+      //          <div id="temp"><span></span></div>
+      //          <div id="desc"></div>
+      //          <div id="hi-low"></div>
+      //          <div id="icon">
+
+      //          </div>
+
+      //      </div>
+      //  </main>
+      //</div>
     );
   }
 }
